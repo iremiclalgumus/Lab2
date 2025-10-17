b25c7eea52af78934d49f63383de2498107db308
İrem İclal Gümüş <s200444071@stu.thk.edu.tr>
2025-10-17 14:31:37 +0300

    new 2

---
+++ src/CheckPrime.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class CheckPrime {
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[]args){
+        Scanner input= new Scanner(System.in);
+        System.out.println("Enter a number");
+        int number= input.nextInt();
+        if(isPrime(number)){
+            System.out.println(number+"is prime");
+
+        }else {
+            System.out.println(number + "is not prime");
+
+        }
+    }
+
+}
